c1c573914d96176df30ee754150dcaef2b865630
zakzhu <zakzhu@outlook.com>
2020-10-30 10:43:42 +0800

    add role alias_profile

---
+++ alias_profile/files/alias.sh
@@ -0,0 +1,14 @@
+# This file is used to include alias.d/*.sh
+# Note: Don't change this file!
+# If you want to create a command alias, please 
+# write a custom.sh shell script in /etc/profile.d/alias.d/
+
+for i in /etc/alias.d/*.sh ; do
+    if [ -r "$i" ]; then
+        if [ "${-#*i}" != "$-" ]; then 
+            . "$i"
+        else
+            . "$i" >/dev/null
+        fi
+    fi
+done
